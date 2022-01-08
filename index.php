6ca79d38807ce1761af8606b341b053d0698d70c
alisa.ofitserova <alisa.ofitserova@yandex.ru>
2022-01-09 00:32:17 +0300

    удаление пользователя

--- index.php
+++ index.php
@@ -56,12 +56,18 @@ $city = $user['city_id'] ? $user['city_id'] : '-';
         <tr>
             <td>{$user['id']}</td>
             <td>{$user['login']}</td>
-            <td>{$user['name']}</td>
+            <td>  
+              <a href='pages/user.php?id={$user['id']}'>
+                 {$user['name']}
+              </a>
+            </td>     
             <td>{$city}</td>
             <td class='text-center'>
-              <form method='post' action=del.user.php'>
-              <button class=btn btn-danger'>x</button>
-            </td>
+              <form method='post' action='actions/del.user.php'>
+                <input type='hidden' name='id' value='{$user['id']}'>
+                  <button type='submite' class='btn btn-danger'>x</button>
+              </form>
+              </td>
         </tr>
     ";
 }
